d694e77526b9945c1f505abd67db42a8823ddc06
Audra Tallman <audi.tallman@gmail.com>
2019-04-14 17:10:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,10 +73,10 @@ attr_accessor :name, :breed, :id
   end   
     
   def self.new_from_db(row)
-    dog = self.new
-    dog.id = row[0]
-    dog.name = row[1]
-    dog.breed = row[2]
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
   end  
   
   def self.find_by_name(name)
